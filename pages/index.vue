60a3dfc05a77037b6fdac19228731deb5dcb19c1
Estifanos Neway <Estifanos.Neway.D@gmail.com>
2023-04-25 17:31:37 +0300

    Created home page UI

--- pages/index.vue
+++ pages/index.vue
@@ -47,9 +47,9 @@
                 </p>
             </div>
             <div v-if="result"
-                class="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                class="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 ">
                 <article v-for="event in result.eventsByLocation" :key="event.id"
-                    class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
+                    class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80 border-2 border-background hover:border-primary">
                     <img :src="createStaticServerLink(event.images ?? defaultEventImageUrl)" :alt="event.title"
                         class="absolute inset-0 -z-10 h-full w-full object-cover" />
                     <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
